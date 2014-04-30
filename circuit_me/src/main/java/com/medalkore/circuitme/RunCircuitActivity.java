8a619dc56047c58f2d267a9fe013cd79ad48cf8c
Andrew Lemmons <andrew@medalkore.com>
2014-04-30 17:45:11 -0500

    Initial commit and Database setup

---
+++ circuit_me/src/main/java/com/medalkore/circuitme/RunCircuitActivity.java
@@ -0,0 +1,27 @@
+package com.medalkore.circuitme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alemmons on 4/29/2014.
+ */
+public class RunCircuitActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_run_circuit);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+}
